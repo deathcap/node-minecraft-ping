e9320ff1c2920bd86cc2b47a51f0bb7dac1fe268
deathcap <deathcap@gmx.co.uk>
2016-01-24 20:48:26 -0800

    Support pre-1.6 ping response, assume is 1.5.2

--- mcping16.js
+++ mcping16.js
@@ -35,6 +35,29 @@ socket.on('data', (raw) => {
 
   const string = raw.slice(4).toString('ucs2');
   console.log('response string',string);
-  const array = string.split('\xa7');
-  console.log('array',array);
+  let result = {};
+
+  if (string[0] == '\xa7') {
+    // 1.6 has more info
+    // https://gist.github.com/Jckf/4574114#file-minecraftserver-java-L170
+    const parts = string.split('\0');
+    result.pingVersion = parseInt(parts[0]);
+    result.protocolVersion = parseInt(parts[1]);
+    result.gameVersion = parts[2];
+    result.motd = parts[3];
+    result.playersOnline = parts[4];
+    result.maxPlayers = parts[5];
+  } else {
+    // http://wiki.vg/Server_List_Ping#1.4_to_1.5
+    // "Prior to the Minecraft 1.6, the client to server operation is much simpler, and only sends FE 01, with none of the following data."
+    // since it could be any version earlier, assume the latest
+    result.gameVersion = '1.5.2';
+    result.protocolVersion = 61; // https://github.com/PrismarineJS/minecraft-data/pull/92/files#diff-0ccb8c9bf6497574bdc134eb428dc649R782
+
+    const parts = string.split('\xa7');
+    result.motd = parts[0];
+    result.playersOnline = parts[1];
+    result.maxPlayers = parts[2];
+  }
+  console.log('result',result);
 });
