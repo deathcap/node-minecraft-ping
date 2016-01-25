174527e26c490d17d67e1bcc2ccd9505990adf77
deathcap <deathcap@gmx.co.uk>
2016-01-24 21:00:25 -0800

    Send the 0xfe 0x01 packet instead of 0xfd, it gives the protocol and game version even on 1.5.2

--- mcping16.js
+++ mcping16.js
@@ -20,7 +20,11 @@ socket.on('connect', () => {
 
   // request challenge token
   // TODO: encode/decode using protodef
-  socket.write(new Buffer('fefd090000000000000000', 'hex'));
+  //socket.write(new Buffer('fefd090000000000000000', 'hex')); // the 2011 beta 1.9 query protocol from https://dinnerbone.com/blog/2011/10/14/minecraft-19-has-rcon-and-query/
+    // http://wiki.vg/Server_List_Ping#1.4_to_1.5
+    // "Prior to the Minecraft 1.6, the client to server operation is much simpler, and only sends FE 01, with none of the following data."
+    // since it could be any version earlier, assume the latest
+  socket.write(new Buffer('fe01', 'hex'));
 });
 socket.on('end', () => {
   console.log('ended');
@@ -38,19 +42,18 @@ socket.on('data', (raw) => {
   let result = {};
 
   if (string[0] == '\xa7') {
-    // 1.6 has more info
     // https://gist.github.com/Jckf/4574114#file-minecraftserver-java-L170
     const parts = string.split('\0');
-    result.pingVersion = parseInt(parts[0]);
+    console.log(parts);
+    result.pingVersion = parseInt(parts[0].slice(1));
     result.protocolVersion = parseInt(parts[1]);
     result.gameVersion = parts[2];
     result.motd = parts[3];
     result.playersOnline = parseInt(parts[4]);
     result.maxPlayers = parseInt(parts[5]);
   } else {
-    // http://wiki.vg/Server_List_Ping#1.4_to_1.5
-    // "Prior to the Minecraft 1.6, the client to server operation is much simpler, and only sends FE 01, with none of the following data."
-    // since it could be any version earlier, assume the latest
+    // 0xfe 0xfd response lacks game version, so assume a reasonably old one
+    result.pingVersion = 0;
     result.gameVersion = '1.5.2';
     result.protocolVersion = 61; // https://github.com/PrismarineJS/minecraft-data/pull/92/files#diff-0ccb8c9bf6497574bdc134eb428dc649R782
 
