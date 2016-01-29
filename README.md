2e1d7a5de2d68f9488c64a655eeceb2b9b809272
deathcap <deathcap@gmx.co.uk>
2016-01-28 22:06:27 -0800

    Rename to node-minecraft-ping because FE01 ping also works on 1.7/8/9. Closes GH-3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# mcping16
+# node-minecraft-ping
 
-Sends a [server list ping](http://wiki.vg/Server_List_Ping#1.6) packet to pre-Netty Minecraft servers (1.6.4, 1.5.2, 1.4.4)
+Sends a [server list ping](http://wiki.vg/Server_List_Ping#1.6) packet Minecraft servers (supports 1.4.4+)
 
-For 1.7+ and later, try [node-minecraft-protocol](https://github.com/PrismarineJS/node-minecraft-protocol)'s ping instead.
+For 1.7+ and later, also try [node-minecraft-protocol](https://github.com/PrismarineJS/node-minecraft-protocol)'s ping.
 
 Usage:
 
--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const mcping16 = require('./');
+const mcping = require('./');
 
 if (process.argv.length < 4) {
   console.log('Usage: node demo.js <host> <port>');
@@ -10,7 +10,7 @@ if (process.argv.length < 4) {
 const host = process.argv[2];
 const port = parseInt(process.argv[3]);
 
-mcping16.ping_fe01({host, port}, function(err, response) {
+mcping.ping_fe01({host, port}, function(err, response) {
   if (err) {
     console.log('ping_fe01 error',err);
     return;
@@ -19,7 +19,7 @@ mcping16.ping_fe01({host, port}, function(err, response) {
   console.log('received ping_fe01',response);
 });
 
-mcping16.ping_fefd_udp({host, port}, function(err, response) {
+mcping.ping_fefd_udp({host, port}, function(err, response) {
   if (err) {
     console.log('ping_fefd error',err);
     return;
